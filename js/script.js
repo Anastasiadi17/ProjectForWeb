ce9eb50221e231a75c8bc0bc360c49791fdcf6d1
Anastasiadi17 <90448892+Anastasiadi17@users.noreply.github.com>
2022-01-31 20:51:03 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function moveNavbar(){
+  let windowInnerWidth = window.innerWidth;
+  let navbar = $("#navbar");
+  if(windowInnerWidth <= 993) {
+      navbar.addClass("fixed-bottom");
+      navbar.addClass("bg-dark");
+  }
+  if(windowInnerWidth >= 993){
+      navbar.removeClass("fixed-bottom");
+      navbar.removeClass("bg-dark");
+  }
+}
+
+function showDropdown(navbarDropdown, list){
+  $(navbarDropdown).addClass("show");
+  $(navbarDropdown).prop("aria-expanded", true);
+  $(list).addClass("show");
+}
+
+function hideDropdown(navbarDropdown, list){
+  let windowInnerWidth = window.innerWidth;
+  if(windowInnerWidth >= 993) {
+      $(navbarDropdown).removeClass("show");
+      $(navbarDropdown).prop("aria-expanded", false);
+      $(list).removeClass("show");
+  }
+}
